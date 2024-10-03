50e6b7b2e08aae3ffd0683f3eeefb770d85aee39
Vinícius da Veiga <1028313@UNIDAVI.LOCAL>
2024-10-03 20:04:56 -0300

    terceira parte, adicionado e melhorado a página SOBRE com respondividade na navbar

--- main.js
+++ main.js
@@ -3,37 +3,32 @@ const navLinks = document.getElementById('nav-links');
 const navItems = document.querySelectorAll('#nav-links li a');
 const closeBtn = document.getElementById('close-btn');
 
-// Alterna o menu ao clicar no ícone
 menuIcon.addEventListener('click', (e) => {
-    e.stopPropagation(); // Impede o clique de se propagar e fechar o menu ao clicar no ícone
+    e.stopPropagation(); 
     navLinks.classList.toggle('active');
 });
 
-// Fecha o menu quando qualquer link for clicado
 navItems.forEach(link => {
     link.addEventListener('click', () => {
         navLinks.classList.remove('active');
     });
 });
 
-// Fecha o menu ao clicar fora dele
 document.addEventListener('click', (e) => {
     if (!navLinks.contains(e.target) && !menuIcon.contains(e.target)) {
         navLinks.classList.remove('active');
     }
 });
 
-// Fecha o menu ao clicar no botão "X"
 closeBtn.addEventListener('click', (e) => {
-    e.stopPropagation(); // Impede que o clique no "X" feche o menu ao clicar fora
-    navLinks.classList.remove('active'); // Fecha o menu
+    e.stopPropagation(); 
+    navLinks.classList.remove('active'); 
 });
 
-// Rolar para o topo quando clicar no logo
 document.getElementById('logo').addEventListener('click', (e) => {
     e.preventDefault();
     window.scrollTo({
         top: 0,
-        behavior: 'smooth' // Scroll suave
+        behavior: 'smooth'
     });
 });
