4961d8e24497059458443053eba9bde680350934
amandioah <amandioah@gmail.com>
2024-10-31 20:10:29 -0300

    Iniciando as configurações do PWA

--- main.js
+++ main.js
@@ -32,3 +32,16 @@ document.getElementById('logo').addEventListener('click', (e) => {
         behavior: 'smooth'
     });
 });
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/service-worker.js')
+        .then((registration) => {
+          console.log('Service Worker registrado com sucesso:', registration);
+        })
+        .catch((error) => {
+          console.log('Falha ao registrar o Service Worker:', error);
+        });
+    });
+  }
+  
